cdef47dfe2a377e51db087980d9825f27b35d367
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-26 23:58:15 -0400

    style: Definição de cores root do projeto e add Header Style e ícones)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom"
+
+import { GiReceiveMoney, GiHamburgerMenu } from "react-icons/gi"
+import { FaUserCircle } from "react-icons/fa"
+
 import "./Header.css"
 
 export function Header() {
     return (
         <header className="cabecalho">
-            <h1>Controle de Pagamentos</h1>
+            <GiHamburgerMenu className="icones__nav menu_nav"/>
+            <figure className="container__logo">
+                <h1 className="titulo__cabecalho">Controle de Pagamentos</h1>
+                <GiReceiveMoney className="icone__app"/>
+            </figure>
+            <FaUserCircle className="icones__nav"/>
+            <nav className="menu__nav">
+                <Link to={"/"}>Home</Link>
+            </nav>
         </header>
     )
 }
