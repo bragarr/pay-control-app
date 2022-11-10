2a88bfccd58892e40a7f8a42cb6d6c0803b2a34a
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-11-09 23:36:36 -0500

    feat: Regra excp para somente usuários logados no sistema podem acessar cadastros e pagamentos

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 
+import { useAuth } from "../Hooks/useAuth";
+import { auth } from "../contexts/Firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 import { Home } from "../pages/Home/Home";
 import { Cadastros } from "../pages/Cadastros/Cadastros"
 import { Pagamentos } from "../pages/Pagamentos/Pagamentos"
 
 export function MainRoutes() {
+
+    const AreaReservadaCadastros = (parametro) => {
+        const { logado } = useAuth();
+
+        return logado > 0 ? <Cadastros /> : <Home /> 
+    }
+
+    const AreaReservadaPagamentos = (parametro) => {
+        const { logado } = useAuth();
+
+        return logado > 0 ? <Pagamentos /> : <Home /> 
+    }
+
     return(
         <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path="cadastros" element={<Cadastros />}></Route>
-            <Route path="pagamentos" element={<Pagamentos />}></Route>
+            <Route path="cadastros" element={<AreaReservadaCadastros Item={Cadastros} />}></Route>
+            <Route path="pagamentos" element={<AreaReservadaPagamentos Item={Pagamentos} />}></Route>
         </Routes>
     )
 }
