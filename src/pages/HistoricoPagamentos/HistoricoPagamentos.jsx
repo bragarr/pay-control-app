f4c03e20c741bf0ecc78b2e6ef92d26015bd4df5
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-11-20 23:12:26 -0400

    style: Ajuste de chamada de dados da API e padronização de sistema de classses de estilização

--- src/pages/HistoricoPagamentos/HistoricoPagamentos.jsx
+++ src/pages/HistoricoPagamentos/HistoricoPagamentos.jsx
@@ -24,25 +24,26 @@ export function HistoricoPagamentos() {
     return(
         <section>
             <h2>Histórico de Pagamentos</h2>
-            <table className="table__users">
-            <thead className="table__head">
-                <tr className="fields__table">
-                    <th className="info__inputs">Tipo Pagamento</th>
-                    <th className="info__inputs">Nome</th>
-                    <th className="info__inputs input__fone">Valor</th>
-                    <th className="info__inputs input__tag">Obs/Justificativa</th>
-                    <th className="info__actions">Editar</th>
-                    <th className="info__actions">Deletar</th>
+            <table>
+            <thead>
+                <tr>
+                    <th>Tipo Pagamento</th>
+                    <th>Nome</th>
+                    <th>Valor</th>
+                    <th>Obs/Justificativa</th>
+                    <th>Editar</th>
+                    <th>Deletar</th>
 
                 </tr>
             </thead>
-            <tbody className="table__body">
+            <tbody>
                 {cadastrados.map((item, i) => (
                     <tr key={i}>
-                        <td className="info__inputs info__body">Tipo Pagamento</td>
-                        <td className="info__inputs info__body input__email">Nome</td>
-                        <td className="info__inputs info__body input__fone">Valor</td>
-                        <td className="info__inputs info__body input__tag--desc">Obs/Justificativa</td>
+                        <td>{item.tipo_pagamento}</td>
+                        <td>{item.nome}</td>
+                        <td>R${item.valor_pagamento}</td>
+                        <td>{item.obs}</td>
+                        <td>{item.data_pagamento}</td>
                         <td>
                             Editar
                         </td>
